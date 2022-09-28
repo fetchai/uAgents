7ef12b2e937365103828775221a974197d1a6819
Ed FitzGerald <ejafitzgerald@gmail.com>
2022-09-28 21:02:11 +0100

    chore: fix pylint issues

--- examples/simple/main.py
+++ examples/simple/main.py
@@ -4,12 +4,12 @@ agent = Agent()
 
 
 @agent.on_interval(period=1.0)
-async def on_interval():
+async def on_interval1():
     print("User interval code 1")
 
 
 @agent.on_interval(period=3.0)
-async def on_interval():
+async def on_interval2():
     print("User interval code 3")
 
 
