eb9f9903f17ad50165d8852c2098582081edced3
Ed FitzGerald <edward.fitzgerald@fetch.ai>
2022-11-02 20:57:31 +0000

    fix: correct agent examples (#4)

--- examples/simple/main.py
+++ examples/simple/main.py
@@ -1,15 +1,15 @@
-from nexus import Agent
+from nexus import Agent, Context
 
 agent = Agent()
 
 
 @agent.on_interval(period=1.0)
-async def on_interval1():
+async def on_interval1(_ctx: Context):
     print("User interval code 1")
 
 
 @agent.on_interval(period=3.0)
-async def on_interval2():
+async def on_interval2(_ctx: Context):
     print("User interval code 3")
 
 
