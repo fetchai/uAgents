fd222f2a428fbc2911e8a5c0fac5ff43deed4035
Ed FitzGerald <ejafitzgerald@gmail.com>
2022-11-02 20:52:19 +0000

    feat: add basic storage example

---
+++ examples/simple-storage/main.py
@@ -0,0 +1,16 @@
+from nexus import Agent, Context
+
+agent = Agent()
+
+
+@agent.on_interval(period=1.0)
+async def on_interval(ctx: Context):
+    current_count = ctx.storage.get('count') or 0
+
+    print(f"My count is: {current_count}")
+
+    ctx.storage.set('count', current_count + 1)
+
+
+if __name__ == "__main__":
+    agent.run()
