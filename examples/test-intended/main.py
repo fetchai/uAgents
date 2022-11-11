7380cb9112e575ecf1a5514615e5d47b5ed832fb
Alejandro-Morales <alejandro.morales.mad@gmail.com>
2022-11-11 05:34:01 -0600

    feat: added crypto logic

---
+++ examples/test-intended/main.py
@@ -0,0 +1,70 @@
+from nexus import Agent, Bureau, Context, Model
+import hashlib
+
+
+class Message(Model):
+    message: str
+    digest: bytes
+    signature: str
+
+
+def encode(message: str) -> bytes:
+    hasher = hashlib.sha256()
+    hasher.update(message.encode())
+    return hasher.digest()
+
+
+alice = Agent(name="alice", seed="alice recovery password")
+bob = Agent(name="bob", seed="bob recovery password")
+
+assert (
+    alice.address == "agent1qg985el6kquqw3zdnq7tvpsz2n0srxa8e8eyp0nwpagp6yyg2ayus294dux"
+)
+assert (
+    bob.address == "agent1qgqdlpds2w7rs032jgylcka8m5d663nvt6p5dmmffpmanljldchssyjhn9r"
+)
+
+
+@alice.on_interval(period=3.0)
+async def send_message(ctx: Context):
+    # chance funcion
+    msg = "hello there bob"
+    digest = encode(msg)
+    await ctx.send(
+        bob.address,
+        Message(
+            message=msg, digest=digest, signature=alice._identity.sign_digest(digest)
+        ),
+    )
+
+
+@alice.on_message(model=Message)
+async def alice_rx_message(ctx: Context, sender: str, msg: Message):
+    assert alice._identity.verify_digest(sender, msg.digest, msg.signature), "couldn't verify bob's message"
+    print(f"[{ctx.name:5}] From: {sender} {msg.message}")
+
+
+@bob.on_message(model=Message)
+async def bob_rx_message(ctx: Context, sender: str, msg: Message):
+    assert bob._identity.verify_digest(sender, msg.digest, msg.signature), "couldn't verify alice's message"
+    print(f"[{ctx.name:5}] From: {sender} {msg.message}")
+
+    msg = "hello there alice"
+    digest = encode(msg)
+
+    # send the response
+    await ctx.send(
+        alice.address,
+        Message(
+            message=msg, digest=digest, signature=bob._identity.sign_digest(digest)
+        ),
+    )
+
+# since we have multiple agents in this example we add them to a bureau
+# (just an "office" for agents)
+bureau = Bureau()
+bureau.add(alice)
+bureau.add(bob)
+
+if __name__ == "__main__":
+    bureau.run()
