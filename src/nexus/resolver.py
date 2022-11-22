1fe1955dd757a078b27f105067287be6ec3334e5
James Riehl <james.riehl@fetch.ai>
2022-11-22 09:22:19 +0000

    fix: agent and wallet key derivation

--- src/nexus/resolver.py
+++ src/nexus/resolver.py
@@ -1,14 +1,12 @@
 from abc import ABC, abstractmethod
 from typing import Dict, Optional
 
-from nexus.crypto import Identity
-from nexus.network import get_ledger, get_reg_contract, get_wallet
+from nexus.network import get_reg_contract
 
 
-def _query_record(wallet_address: str, service: str) -> dict:
-    ledger = get_ledger("fetchai-testnet")
-    contract = get_reg_contract(ledger)
-    query_msg = {"query_record": {"address": wallet_address, "record_type": service}}
+def _query_record(agent_address: str, service: str) -> dict:
+    contract = get_reg_contract()
+    query_msg = {"query_record": {"address": agent_address, "record_type": service}}
     result = contract.query(query_msg)
     return result
 
@@ -21,8 +19,7 @@ class Resolver(ABC):
 
 class AlmanacResolver(Resolver):
     async def resolve(self, address: str) -> str:
-        wallet = get_wallet(Identity.get_key(address))
-        result = _query_record(wallet.address(), "Service")
+        result = _query_record(address, "Service")
         if result is not None:
             record = result.get("record") or {}
             endpoints = record.get("record", {}).get("Service", {}).get("endpoints", [])
--- src/nexus/setup.py
+++ src/nexus/setup.py
@@ -1,20 +1,17 @@
 import logging
 
-from cosmpy.aerial.client import NetworkConfig
-from cosmpy.aerial.faucet import FaucetApi
-
 from nexus.config import REGISTRATION_FEE
-from nexus.network import get_ledger
+from nexus.network import get_ledger, get_faucet
 
 
 def fund_agent_if_low(agent_address: str):
-    ledger = get_ledger("fetchai-testnet")
-    faucet_api = FaucetApi(NetworkConfig.latest_stable_testnet())
+    ledger = get_ledger()
+    faucet = get_faucet()
 
     agent_balance = ledger.query_bank_balance(agent_address)
 
     if agent_balance < REGISTRATION_FEE:
         # Add tokens to agent's wallet
         logging.info("Adding funds to agent...")
-        faucet_api.get_wealth(agent_address)
+        faucet.get_wealth(agent_address)
         logging.info("Adding funds to agent...complete")
