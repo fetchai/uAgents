8c648ddb417a85da061302103511b7442f7935dc
James Riehl <james.riehl@fetch.ai>
2023-01-05 17:55:28 +0000

    chore: revert to bech32 signature

--- examples/10-cleaning-demo/cleaner.py
+++ examples/10-cleaning-demo/cleaner.py
@@ -44,8 +44,8 @@ async def startup():
 
     await Availability.create(
         provider=provider,
-        time_start=utc.localize(datetime.fromisoformat("2022-12-31 12:00:00")),
-        time_end=utc.localize(datetime.fromisoformat("2023-01-01 00:00:00")),
+        time_start=utc.localize(datetime.fromisoformat("2022-01-31 00:00:00")),
+        time_end=utc.localize(datetime.fromisoformat("2023-02-01 00:00:00")),
         max_distance=10,
         min_hourly_price=5,
     )
