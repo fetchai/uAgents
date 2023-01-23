ac65b692edb6ed5b71d05e26a42859a41b414067
Alejandro-Morales <alejandro.morales.mad@gmail.com>
2023-01-23 07:14:20 -0600

    fix: ex 8 recipient address

--- examples/08-remote-agents-registration/agent2.py
+++ examples/08-remote-agents-registration/agent2.py
@@ -17,6 +17,8 @@ agent = Agent(
 
 fund_agent_if_low(agent.wallet.address())
 
+print(agent.address)
+
 
 @agent.on_message(model=Message)
 async def bob_rx_message(ctx: Context, sender: str, msg: Message):
