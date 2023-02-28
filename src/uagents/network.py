d1740e3d4297f5e2ab70e6d3d1194e720c595b82
James Riehl <james.riehl@fetch.ai>
2023-02-28 17:18:37 +0000

    feat: add prepare tx function

--- src/uagents/network.py
+++ src/uagents/network.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 from cosmpy.aerial.contract import LedgerContract
 from cosmpy.aerial.client import (
+    Account,
     LedgerClient,
     NetworkConfig,
     DEFAULT_QUERY_INTERVAL_SECS,
@@ -11,7 +12,9 @@ from cosmpy.aerial.client import (
 )
 from cosmpy.aerial.exceptions import NotFoundError, QueryTimeoutError
 from cosmpy.aerial.faucet import FaucetApi
-from cosmpy.aerial.tx_helpers import TxResponse
+from cosmpy.aerial.tx import SigningCfg, Transaction
+from cosmpy.aerial.tx_helpers import TxResponse, SubmittedTx
+from cosmpy.aerial.wallet import Wallet
 
 from uagents.config import AgentNetwork, CONTRACT_ALMANAC, AGENT_NETWORK
 
@@ -60,3 +63,40 @@ async def wait_for_tx_to_complete(
             raise QueryTimeoutError()
 
         await asyncio.sleep(poll_period.total_seconds())
+
+
+def prepare_basic_transaction(
+    client: LedgerClient,
+    tx: Transaction,
+    sender: Wallet,
+    account: Optional[Account] = None,
+    gas_limit: Optional[int] = None,
+    memo: Optional[str] = None,
+) -> Transaction:
+    if account is None:
+        account = client.query_account(sender.address())
+
+    if gas_limit is not None:
+        fee = client.estimate_fee_from_gas(gas_limit)
+    else:
+        tx.seal(
+            SigningCfg.direct(sender.public_key(), account.sequence),
+            fee="",
+            gas_limit=0,
+            memo=memo,
+        )
+        tx.sign(sender.signer(), client.network_config.chain_id, account.number)
+        tx.complete()
+
+        # simulate the gas and fee for the transaction
+        gas_limit, fee = client.estimate_gas_and_fee_for_tx(tx)
+
+    # finally, build the final transaction that will be executed with the correct gas and fee values
+    tx.seal(
+        SigningCfg.direct(sender.public_key(), account.sequence),
+        fee=fee,
+        gas_limit=gas_limit,
+        memo=memo,
+    )
+
+    return tx
