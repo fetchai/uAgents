a103aff9a11d220a8de259b6e0cb8b334ea2affe
James Riehl <james.riehl@fetch.ai>
2023-06-22 13:34:00 +0100

    test: add tests for agentverse config

---
+++ tests/test_config.py
@@ -0,0 +1,117 @@
+import unittest
+
+from uagents import Agent
+from uagents.config import parse_agentverse_config
+
+
+agents = [
+    Agent(),
+    Agent(mailbox="api_key@some_url"),
+    Agent(mailbox={"api_key": "api_key", "base_url": "some_url"}),
+    Agent(mailbox="api_key"),
+    Agent(agentverse="api_key@some_url"),
+    Agent(agentverse="api_key"),
+    Agent(agentverse="http://some_url"),
+    Agent(agentverse="wss://some_url"),
+    Agent(agentverse="ws://some_url"),
+    Agent(agentverse={"api_key": "api_key", "protocol": "wss"}),
+    Agent(agentverse="https://staging.agentverse.ai"),
+    Agent(agentverse={"base_url": "staging.agentverse.ai"}),
+]
+
+expected_configs = [
+    {
+        "api_key": None,
+        "base_url": "agentverse.ai",
+        "protocol": "https",
+        "http_prefix": "https",
+        "use_mailbox": False,
+    },
+    {
+        "api_key": "api_key",
+        "base_url": "some_url",
+        "protocol": "https",
+        "http_prefix": "https",
+        "use_mailbox": True,
+    },
+    {
+        "api_key": "api_key",
+        "base_url": "some_url",
+        "protocol": "https",
+        "http_prefix": "https",
+        "use_mailbox": True,
+    },
+    {
+        "api_key": "api_key",
+        "base_url": "agentverse.ai",
+        "protocol": "https",
+        "http_prefix": "https",
+        "use_mailbox": True,
+    },
+    {
+        "api_key": "api_key",
+        "base_url": "some_url",
+        "protocol": "https",
+        "http_prefix": "https",
+        "use_mailbox": True,
+    },
+    {
+        "api_key": "api_key",
+        "base_url": "agentverse.ai",
+        "protocol": "https",
+        "http_prefix": "https",
+        "use_mailbox": True,
+    },
+    {
+        "api_key": None,
+        "base_url": "some_url",
+        "protocol": "http",
+        "http_prefix": "http",
+        "use_mailbox": False,
+    },
+    {
+        "api_key": None,
+        "base_url": "some_url",
+        "protocol": "wss",
+        "http_prefix": "https",
+        "use_mailbox": False,
+    },
+    {
+        "api_key": None,
+        "base_url": "some_url",
+        "protocol": "ws",
+        "http_prefix": "http",
+        "use_mailbox": False,
+    },
+    {
+        "api_key": "api_key",
+        "base_url": "agentverse.ai",
+        "protocol": "wss",
+        "http_prefix": "https",
+        "use_mailbox": True,
+    },
+    {
+        "api_key": None,
+        "base_url": "staging.agentverse.ai",
+        "protocol": "https",
+        "http_prefix": "https",
+        "use_mailbox": False,
+    },
+    {
+        "api_key": None,
+        "base_url": "staging.agentverse.ai",
+        "protocol": "https",
+        "http_prefix": "https",
+        "use_mailbox": False,
+    },
+]
+
+
+for agent in agents:
+    print(agent.agentverse)
+
+
+class TestConfig(unittest.TestCase):
+    def test_parse_agentverse_config(self):
+        for agent, expected_config in zip(agents, expected_configs):
+            self.assertEqual(agent.agentverse, expected_config)
