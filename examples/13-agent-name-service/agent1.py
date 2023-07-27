b8569a5471ad6c297e52f55d581113a7093d91b5
Alejandro-Morales <77800944+Alejandro-Morales@users.noreply.github.com>
2023-07-27 07:49:04 -0600

    feat: new almanac and name service registration flow (#114)
    Co-authored-by: James Riehl <33920192+jrriehl@users.noreply.github.com>
    Co-authored-by: James Riehl <james.riehl@fetch.ai>

---
+++ examples/13-agent-name-service/agent1.py
@@ -0,0 +1,44 @@
+from cosmpy.aerial.wallet import LocalWallet
+
+from uagents.network import get_ledger, get_name_service_contract
+from uagents.setup import fund_agent_if_low
+from uagents import Agent, Context, Model
+
+
+# NOTE: Run agent1.py before running agent2.py
+
+
+class Message(Model):
+    message: str
+
+
+bob = Agent(
+    name="bob-0",
+    seed="agent bob-0 secret phrase",
+    port=8001,
+    endpoint=["http://localhost:8001/submit"],
+)
+
+ledger = get_ledger()
+my_wallet = LocalWallet.from_unsafe_seed("registration test wallet")
+name_service_contract = get_name_service_contract()
+DOMAIN = "agent"
+
+for wallet in [my_wallet, bob.wallet]:
+    fund_agent_if_low(wallet.address())
+
+
+@bob.on_event("startup")
+async def register_agent_name(ctx: Context):
+    await name_service_contract.register(
+        ledger, my_wallet, ctx.address, ctx.name, DOMAIN
+    )
+
+
+@bob.on_message(model=Message)
+async def message_handler(ctx: Context, sender: str, msg: Message):
+    ctx.logger.info(f"Received message from {sender}: {msg.message}")
+
+
+if __name__ == "__main__":
+    bob.run()
