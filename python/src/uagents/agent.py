3c41113999ea345abfc0cbe468becce90d1ab720
James Riehl <33920192+jrriehl@users.noreply.github.com>
2023-08-31 10:26:18 +0100

    feat: better exception handling (#133)

--- python/src/uagents/agent.py
+++ python/src/uagents/agent.py
@@ -2,6 +2,7 @@ import asyncio
 import functools
 from typing import Dict, List, Optional, Set, Union, Type, Tuple, Any, Coroutine
 import uuid
+from pydantic import ValidationError
 import requests
 
 from cosmpy.aerial.wallet import LocalWallet, PrivateKey
@@ -41,10 +42,12 @@ async def _run_interval(func: IntervalCallback, ctx: Context, period: float):
     while True:
         try:
             await func(ctx)
-        except OSError:
-            ctx.logger.exception("OS Error in interval handler")
-        except RuntimeError:
-            ctx.logger.exception("Runtime Error in interval handler")
+        except OSError as ex:
+            ctx.logger.exception(f"OS Error in interval handler: {ex}")
+        except RuntimeError as ex:
+            ctx.logger.exception(f"Runtime Error in interval handler: {ex}")
+        except Exception as ex:
+            ctx.logger.exception(f"Exception in interval handler: {ex}")
 
         await asyncio.sleep(period)
 
@@ -386,11 +389,25 @@ class Agent(Sink):
     async def _startup(self):
         await self._registration_loop()
         for handler in self._on_startup:
-            await handler(self._ctx)
+            try:
+                await handler(self._ctx)
+            except OSError as ex:
+                self._logger.exception(f"OS Error in startup handler: {ex}")
+            except RuntimeError as ex:
+                self._logger.exception(f"Runtime Error in startup handler: {ex}")
+            except Exception as ex:
+                self._logger.exception(f"Exception in startup handler: {ex}")
 
     async def _shutdown(self):
         for handler in self._on_shutdown:
-            await handler(self._ctx)
+            try:
+                await handler(self._ctx)
+            except OSError as ex:
+                self._logger.exception(f"OS Error in shutdown handler: {ex}")
+            except RuntimeError as ex:
+                self._logger.exception(f"Runtime Error in shutdown handler: {ex}")
+            except Exception as ex:
+                self._logger.exception(f"Exception in shutdown handler: {ex}")
 
     def setup(self):
         # register the internal agent protocol
@@ -436,7 +453,11 @@ class Agent(Sink):
                 continue
 
             # parse the received message
-            recovered = model_class.parse_raw(message)
+            try:
+                recovered = model_class.parse_raw(message)
+            except ValidationError as ex:
+                self._logger.warning(f"Unable to parse message: {ex}")
+                continue
 
             context = Context(
                 self._identity.address,
@@ -475,7 +496,14 @@ class Agent(Sink):
                     continue
 
             if handler is not None:
-                await handler(context, sender, recovered)
+                try:
+                    await handler(context, sender, recovered)
+                except OSError as ex:
+                    self._logger.exception(f"OS Error in message handler: {ex}")
+                except RuntimeError as ex:
+                    self._logger.exception(f"Runtime Error in message handler: {ex}")
+                except Exception as ex:
+                    self._logger.exception(f"Exception in message handler: {ex}")
 
 
 class Bureau:
