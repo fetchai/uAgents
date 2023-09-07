da5e2b77d2e73885a02ddf599e43ca2b3e5ef48d
James Riehl <james.riehl@fetch.ai>
2023-09-07 13:11:13 +0100

    fix: change self.logger to self._logger

--- python/src/uagents/agent.py
+++ python/src/uagents/agent.py
@@ -794,7 +794,7 @@ class Agent(Sink):
             # lookup the model definition
             model_class: Model = self._models.get(schema_digest)
             if model_class is None:
-                self.logger.warning(
+                self._logger.warning(
                     f"Received message with unrecognized schema digest: {schema_digest}"
                 )
                 continue
