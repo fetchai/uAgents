1f8503841f00c9af79e7a8712c72297b8cde927e
James Riehl <james.riehl@fetch.ai>
2023-09-07 13:03:10 +0100

    fix: remove error handler for unrecognized schema digest

--- python/src/uagents/agent.py
+++ python/src/uagents/agent.py
@@ -813,14 +813,7 @@ class Agent(Sink):
             # lookup the model definition
             model_class: Model = self._models.get(schema_digest)
             if model_class is None:
-                await _handle_error(
-                    context,
-                    sender,
-                    ErrorMessage(
-                        error="I do not know how to handle messages with schema digest: "
-                        + schema_digest
-                    ),
-                )
+                self.logger.warning(f"Received message with unrecognized schema digest: {schema_digest}")
                 continue
 
             # parse the received message
