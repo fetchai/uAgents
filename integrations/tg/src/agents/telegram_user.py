04841ad4194edf004d763b6f0102987211a38906
Alexander Ekzarov <ekzarov@gmail.com>
2023-10-01 11:16:30 +0300

    Code cleanup

--- integrations/tg/src/agents/telegram_user.py
+++ integrations/tg/src/agents/telegram_user.py
@@ -3,7 +3,7 @@ from messages.basic import TelegramRequest
 from uagents.setup import fund_agent_if_low
 
 INPUT_TEXT = "This is a test message for telegram sent by fetch agent."
-AI_MODEL_AGENT_ADDRESS = "agent1qdesxanj9s5l9xk4f6nsqqp2k93l7un859s0q3mrcwftxpe60xjg5crcf38"
+TELEGRAM_AGENT_ADDRESS = "agent1qdesxanj9s5l9xk4f6nsqqp2k93l7un859s0q3mrcwftxpe60xjg5crcf38"
 
 user_agent = Agent(
     name="telegram_user",
@@ -18,8 +18,8 @@ user_agent_protocol = Protocol("Request")
 
 @user_agent_protocol.on_interval(360, messages=TelegramRequest)
 async def text_classification(ctx: Context):
-    ctx.logger.info(f"Asking AI model agent to classify: {INPUT_TEXT}")
-    await ctx.send(AI_MODEL_AGENT_ADDRESS, TelegramRequest(text=INPUT_TEXT, chat_id=2032178797))
+    ctx.logger.info(f"Sending message to telegram: {INPUT_TEXT}")
+    await ctx.send(TELEGRAM_AGENT_ADDRESS, TelegramRequest(text=INPUT_TEXT, chat_id=2032178797))
 
 user_agent.include(user_agent_protocol)
 
