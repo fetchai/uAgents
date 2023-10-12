1137bcb8a6c91f305cfaf803224de8d5f723e595
Archento <crossmail@gmx.de>
2023-10-12 15:03:41 +0200

    restructure again + modify chitchat

---
+++ python/examples/15-chit-chat-dialogue/dialogues/chitchat.py
@@ -0,0 +1,72 @@
+"""Specific dialogue class for the chit-chat dialogue."""
+from uagents.experimental.dialogues import Dialogue, Node, Edge
+
+
+class ChitChatDialogue(Dialogue):
+    """
+    This is the specific definition of the rules for the chit-chat dialogue
+    The rules will be predefined and the actual messages will be passed into it
+    """
+
+    state1 = Node(
+        name="Initiated",
+        description="This is the initial state of the dialogue.",
+    )
+    state2 = Node(
+        name="Chit Chatting",
+        description="This is the state in which messages are exchanged.",
+    )
+    state3 = Node(
+        name="Concluded",
+        description="This is the state after the dialogue has been concluded.",
+    )
+
+    # Models are attached to the transitions, this way we may be able to
+    # predefine a specific message structure that can also be overwritten
+    transition1 = Edge(
+        name="Session Started",
+        description="This is the transition from initiated to chit chatting.",
+        parent=state1,
+        child=state2,
+    )
+    transition2 = Edge(
+        name="Session Rejected / Not Needed",
+        description=(
+            "This is the transition for when the session is rejected or not needed."
+        ),
+        parent=state1,
+        child=state3,
+    )
+    transition3 = Edge(
+        name="Dialogue Continues",
+        description=(
+            "This is the transition from one dialogue message to the next, "
+            "i.e. for when the dialogue continues."
+        ),
+        parent=state2,
+        child=state2,
+    )
+    transition4 = Edge(
+        name="Hang Up / End Session",
+        description="This is the transition for when the session is ended.",
+        parent=state2,
+        child=state3,
+    )
+
+    def __init__(
+        self,
+        version: str | None = None,
+        agent_address: str | None = None,
+    ) -> None:
+        super().__init__(
+            name="ChitChatDialogue",
+            version=version,
+            agent_address=agent_address,
+            nodes=[self.state1, self.state2, self.state3],
+            edges=[
+                self.transition1,
+                self.transition2,
+                self.transition3,
+                self.transition4,
+            ],
+        )
---
+++ python/examples/15-chit-chat-dialogue/main.py
@@ -0,0 +1,165 @@
+"""Chit chat dialogue example"""
+from dialogues.chitchat import ChitChatDialogue
+from uagents import Agent, Bureau, Context, Model
+from uagents.setup import fund_agent_if_low
+
+agent1 = Agent(name="agent1", seed="9876543210000000000")
+agent1._logger.setLevel("DEBUG")
+fund_agent_if_low(agent1.wallet.address())
+
+agent2 = Agent(name="agent2", seed="9876543210000000001")
+agent2._logger.setLevel("DEBUG")
+fund_agent_if_low(agent2.wallet.address())
+
+
+class InitiateChitChatDialogue(Model):
+    pass
+
+
+class ChitChatDialogueMessage(Model):
+    text: str
+
+
+class ConcludeChitChatDialogue(Model):
+    pass
+
+
+chitchat_dialogue1 = ChitChatDialogue(
+    version="0.1",
+    agent_address=agent1.address,
+)
+chitchat_dialogue2 = ChitChatDialogue(
+    version="0.1",
+    agent_address=agent2.address,
+)
+
+# agent1
+
+counter = 0
+
+
+@chitchat_dialogue1.on_state_transition("Session Started", InitiateChitChatDialogue)
+async def start_chitchat(
+    ctx: Context,
+    sender: str,
+    _msg: InitiateChitChatDialogue,
+):
+    # do something when the dialogue is started; e.g. send a message
+    await ctx.send(sender, ChitChatDialogueMessage(text="Hello!"))
+
+
+@chitchat_dialogue1.on_state_transition(
+    "Session Rejected / Not Needed", ConcludeChitChatDialogue
+)
+async def reject_chitchat(
+    ctx: Context,
+    sender: str,
+    _msg: ConcludeChitChatDialogue,
+):
+    # do something when the dialogue is rejected and nothing has been sent yet
+    ctx.logger.info(f"Received conclude message from: {sender}")
+
+
+@chitchat_dialogue1.on_state_transition("Dialogue Continues", ChitChatDialogueMessage)
+async def continue_chitchat(
+    ctx: Context,
+    sender: str,
+    msg: ChitChatDialogueMessage,
+):
+    # do something when the dialogue continues
+    ctx.logger.info(f"Received message: {msg}")
+    global counter
+    if counter < 10:
+        await ctx.send(
+            sender,
+            ChitChatDialogueMessage(text=f"Hello again #{counter}!"),
+        )
+        counter += 1
+    else:
+        await ctx.send(sender, ConcludeChitChatDialogue())
+
+
+@chitchat_dialogue1.on_state_transition(
+    "Hang Up / End Session", ConcludeChitChatDialogue
+)
+async def conclude_chitchat(
+    ctx: Context,
+    sender: str,
+    msg: ConcludeChitChatDialogue,
+):
+    # do something when the dialogue is concluded after messages have been exchanged
+    ctx.logger.info(f"Received conclude message from: {sender}")
+
+
+# agent2
+
+
+@chitchat_dialogue2.on_state_transition("Session Started", InitiateChitChatDialogue)
+async def start_chitchat2(
+    ctx: Context,
+    sender: str,
+    _msg: InitiateChitChatDialogue,
+):
+    # do something when the dialogue is started; e.g. send a message
+    await ctx.send(sender, ChitChatDialogueMessage(text="Hello!"))
+
+
+@chitchat_dialogue2.on_state_transition(
+    "Session Rejected / Not Needed", ConcludeChitChatDialogue
+)
+async def reject_chitchat2(
+    ctx: Context,
+    sender: str,
+    _msg: ConcludeChitChatDialogue,
+):
+    # do something when the dialogue is rejected and nothing has been sent yet
+    ctx.logger.info(f"Received conclude message from: {sender}")
+
+
+@chitchat_dialogue2.on_state_transition("Dialogue Continues", ChitChatDialogueMessage)
+async def continue_chitchat2(
+    ctx: Context,
+    sender: str,
+    msg: ChitChatDialogueMessage,
+):
+    # do something when the dialogue continues
+    ctx.logger.info(f"Received message: {msg}")
+    global counter
+    if counter < 10:
+        await ctx.send(
+            sender,
+            ChitChatDialogueMessage(text=f"Hello again #{counter}!"),
+        )
+        counter += 1
+    else:
+        await ctx.send(sender, ConcludeChitChatDialogue())
+
+
+@chitchat_dialogue2.on_state_transition(
+    "Hang Up / End Session", ConcludeChitChatDialogue
+)
+async def conclude_chitchat2(
+    ctx: Context,
+    sender: str,
+    msg: ConcludeChitChatDialogue,
+):
+    # do something when the dialogue is concluded after messages have been exchanged
+    ctx.logger.info(f"Received conclude message from: {sender}")
+
+
+agent1.include(chitchat_dialogue1)
+agent2.include(chitchat_dialogue2)
+
+
+@agent1.on_event("startup")
+async def start_cycle(ctx: Context):
+    await ctx.send(agent2.address, InitiateChitChatDialogue())
+
+
+if __name__ == "__main__":
+    bureau = Bureau(port=8080, endpoint="http://localhost:8080/submit")
+    bureau.add(agent1)
+    print("Agent 1:", agent1.address)
+    bureau.add(agent2)
+    print("Agent 2:", agent2.address)
+    bureau.run()
