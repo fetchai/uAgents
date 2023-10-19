50a0fdce39b6a614816e4cb2a81c76433da6bd01
Archento <crossmail@gmx.de>
2023-10-19 14:22:56 +0200

    simple chitchat cleanup

--- python/examples/15-chit-chat-dialogue/dialogues/simple_chitchat.py
+++ python/examples/15-chit-chat-dialogue/dialogues/simple_chitchat.py
@@ -2,22 +2,6 @@
 from uagents.experimental.dialogues import Dialogue, Node, Edge
 
 # Node definition for the dialogue states
-waiting_state = Node(
-    name="Waiting",
-    description=(
-        "This is the default state of the dialogue. Every session starts in "
-        "this state and is automatically updated once a transition to start or "
-        "continue a dialogue is triggered."
-    ),
-    starter=True,
-)  # currently not used as states are measured by the edges
-# init_state = Node(
-#     name="Initiated",
-#     description=(
-#         "This is the initial state of the dialogue that is only available at "
-#         "the receiving agent."
-#     ),
-# )
 chatting_state = Node(
     name="Chit Chatting",
     description="While in this state, more messages can be exchanged.",
@@ -29,20 +13,6 @@ end_state = Node(
 )
 
 # Edge definition for the dialogue transitions
-# init_session = Edge(
-#     name="Initiate Session",
-#     description="This is the transition to start the dialogue",
-#     parent=None,
-#     child=init_state,
-# )
-# reject_session = Edge(
-#     name="Session Rejected / Not Needed",
-#     description=(
-#         "This is the transition for when the session is rejected or not needed."
-#     ),
-#     parent=init_state,
-#     child=end_state,
-# )
 start_dialogue = Edge(
     name="Start Dialogue",
     description=(
@@ -50,7 +20,7 @@ start_dialogue = Edge(
         "any information needed to set the context and let the receiver "
         "decide whether to accept or directly end this conversation."
     ),
-    parent=waiting_state,
+    parent=None,
     child=chatting_state,
 )
 cont_dialogue = Edge(
@@ -65,8 +35,8 @@ cont_dialogue = Edge(
 end_dialogue = Edge(
     name="End Dialogue",
     description=(
-        "A final message that can be sent at any time by either party at any"
-        "time to finish this dialogue."
+        "A final message that can be sent at any time by either party "
+        "to finish this dialogue."
     ),
     parent=chatting_state,
     child=end_state,
@@ -85,11 +55,10 @@ class SimpleChitChatDialogue(Dialogue):
         agent_address: str | None = None,
     ) -> None:
         super().__init__(
-            name="ChitChatDialogue",
+            name="ChitChatDialogue_simple",
             version=version,
             agent_address=agent_address,
             nodes=[
-                waiting_state,
                 chatting_state,
                 end_state,
             ],
