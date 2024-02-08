835af8fec1c7bbb7a4ebc13fcd87c0e03b9c7070
Alejandro-Morales <77800944+Alejandro-Morales@users.noreply.github.com>
2024-02-08 08:01:07 -0600

    feat: updates to match aname contract-V2 (#215)

--- python/examples/13-agent-name-service/agent2.py
+++ python/examples/13-agent-name-service/agent2.py
@@ -12,10 +12,12 @@ alice = Agent(
     endpoint=["http://localhost:8000/submit"],
 )
 
+DOMAIN = "example.agent"
+
 
 @alice.on_interval(period=5)
 async def alice_interval_handler(ctx: Context):
-    bob_name = "bob-0.agent"
+    bob_name = "bob-0" + "." + DOMAIN
     ctx.logger.info(f"Sending message to {bob_name}...")
     await ctx.send(bob_name, Message(message="Hello there bob."))
 
