10d9766d2e89f06fd806173c8fb3143ebb728636
dpdrabla <159772288+dpdrabla@users.noreply.github.com>
2024-02-12 12:30:42 +0000

    Update main.py
    adding comments

--- integrations/business finder/src/main.py
+++ integrations/business finder/src/main.py
@@ -1,9 +1,10 @@
+# Importing agents and Bureau Module from uagents library
 from business_detail_agent import business_agent
 from business_finder_agent import business_finder
 from user import user
 from uagents import Bureau
 
-#adding all users to bureau and running it
+# Adding all users to bureau and running it
 if __name__ == "__main__":
     bureau = Bureau(endpoint="http://127.0.0.1:8000/submit", port=8000)
     bureau.add(business_agent)
