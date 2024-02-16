27706c8df625d84d486155df1c26251721ee61ce
dpdrabla <divyankdrabla@gmail.com>
2024-02-16 14:36:23 +0000

    Feat: Live Flight Data Integration

---
+++ integrations/flights_data_integration/src/message/model.py
@@ -0,0 +1,10 @@
+# Import librarires
+from uagents import Model
+
+# Model for flught_data request
+class Code(Model):
+    code : str
+
+# Model for flught_data response
+class Response(Model):
+    response : str
