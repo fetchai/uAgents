5906e09707ae9af604f0b429b40afb3d7e65c452
Dacksus <david.noack@fetch.ai>
2024-03-15 16:09:19 +0100

    wip

--- integrations/seo-agent/wrapper.py
+++ integrations/seo-agent/wrapper.py
@@ -1,11 +1,28 @@
 import os
-from langchain_community.utilities.dataforseo_api_search import DataForSeoAPIWrapper
+from typing import List
+
+from langchain_community.document_loaders import AsyncChromiumLoader
+from langchain_community.document_transformers import BeautifulSoupTransformer
 
 os.environ["DATAFORSEO_LOGIN"] = "nikolay.dimitrov@fetch.ai"
 os.environ["DATAFORSEO_PASSWORD"] = "e1e9ead557069e89"
 
-wrapper = DataForSeoAPIWrapper()
+def startProcess(url: str):
+    result = crawlPage(url)
 
-result = wrapper.run("Recent news on Fetch.ai")
+    print(result)
 
-print(result)
+def getSERP(keywords: List[str]):
+    pass
+
+def crawlPage(url: str):
+    loader = AsyncChromiumLoader([url])
+    html = loader.load()
+
+    bs_transformer = BeautifulSoupTransformer()
+    docs_transformed = bs_transformer.transform_documents(html, tags_to_extract=["span"])
+
+    return docs_transformed[0].page_content[0:500]
+
+if __name__ == "__main__":
+    startProcess("https://fetch.ai")
