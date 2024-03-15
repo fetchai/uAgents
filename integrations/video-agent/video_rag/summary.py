0038031f14d5c27510037ce8ddaed3ef5f8f64c1
Attila Bagoly <b.attila.93@gmail.com>
2024-03-15 20:10:29 +0000

    format

--- integrations/video-agent/video_rag/summary.py
+++ integrations/video-agent/video_rag/summary.py
@@ -16,7 +16,7 @@ cod_prompt = hub.pull("whiteforest/chain-of-density-prompt")
 MODELS = {
     "gpt-4": "gpt-4-turbo-preview",
     "gpt-3.5": "gpt-3.5-turbo",
-    "claude-3": "claude-3-opus-20240229"
+    "claude-3": "claude-3-opus-20240229",
 }
 
 TOKEN_LIMIT = {
@@ -31,11 +31,11 @@ logger = structlog.get_logger("model")
 async def compress(content: str, model: str):
     # Chain inputs with defaults for all but {content}
     cod_chain_inputs = {
-        'content': lambda d: d.get('content'),
-        'content_category': lambda d: d.get('content_category', "Article"),
-        'entity_range': lambda d: d.get('entity_range', '1-3'),
-        'max_words': lambda d: int(d.get('max_words', 120)),
-        'iterations': lambda d: int(d.get('iterations', 3))
+        "content": lambda d: d.get("content"),
+        "content_category": lambda d: d.get("content_category", "Article"),
+        "entity_range": lambda d: d.get("entity_range", "1-3"),
+        "max_words": lambda d: int(d.get("max_words", 120)),
+        "iterations": lambda d: int(d.get("iterations", 3)),
     }
 
     model = MODELS[model]
@@ -47,22 +47,20 @@ async def compress(content: str, model: str):
 
     # 1st chain, showing intermediate results, can async stream
     cod_streamable_chain = (
-        cod_chain_inputs
-        | cod_prompt
-        | llm
-        | SimpleJsonOutputParser()
+        cod_chain_inputs | cod_prompt | llm | SimpleJsonOutputParser()
     )
 
-    cod_final_summary_chain = (
-        cod_streamable_chain
-        | (lambda output: output[-1].get('denser_summary', 'ERR: No "denser_summary" key in last dict'))
+    cod_final_summary_chain = cod_streamable_chain | (
+        lambda output: output[-1].get(
+            "denser_summary", 'ERR: No "denser_summary" key in last dict'
+        )
     )
-    return await cod_final_summary_chain.ainvoke({'content': content})
+    return await cod_final_summary_chain.ainvoke({"content": content})
 
 
 async def summarize_transcript_1batch(transcript: str, model: str):
     model = MODELS[model]
-    
+
     if model.find("gpt") != -1:
         llm = ChatOpenAI(temperature=0, model=model)
     else:
@@ -86,24 +84,27 @@ Make sure you capture all the important information from both articles and make 
 {article2}
 """
 
+
 async def article_merger(articles: List[str], model: str):
     if len(articles) < 2:
         return articles[0]
-    
+
     model = MODELS[model]
 
     if model.find("gpt") != -1:
         llm = ChatOpenAI(temperature=0, model=model)
     else:
         llm = ChatAnthropic(temperature=0, model=model)
-    
+
     prompt = ChatPromptTemplate.from_template(merger_prompt)
 
     chain = prompt | llm
 
     merged_article = articles[0]
     for article in articles[1:]:
-        merged_article = await chain.ainvoke({"article1": merged_article, "article2": article}).content
+        merged_article = await chain.ainvoke(
+            {"article1": merged_article, "article2": article}
+        ).content
     return merged_article
 
 
@@ -140,7 +141,9 @@ async def answer_question(article: str, question: str, model: str):
 async def summarize_transcript(transcript: str, model: str):
     num_tokens, batches = get_batches(transcript, TOKEN_LIMIT[model])
 
-    logger.info(f"Got transcript! Model {model}. Total token number: {num_tokens}! Number of batches: {len(batches)}")
+    logger.info(
+        f"Got transcript! Model {model}. Total token number: {num_tokens}! Number of batches: {len(batches)}"
+    )
 
     articles = []
     for i, batch in enumerate(batches):
--- integrations/video-agent/video_rag/utils.py
+++ integrations/video-agent/video_rag/utils.py
@@ -9,7 +9,7 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 logger = structlog.get_logger("utils")
 
-nlp = spacy.load('en_core_web_lg', disable=['parser', 'ner'])
+nlp = spacy.load("en_core_web_lg", disable=["parser", "ner"])
 spacy_words = set(nlp.vocab.strings)
 
 
@@ -19,12 +19,12 @@ def cleaning_fix_unwanted_space_v2(_inp_str: str) -> str:
     _inp_str_splitted = _inp_str.split()
     out_words = []
 
-    i = 0    
+    i = 0
     while i < len(_inp_str_splitted):
         word = _inp_str_splitted[i]
 
-        if word not in _vacob and i+1 < len(_inp_str_splitted):
-            next_word = _inp_str_splitted[i+1]
+        if word not in _vacob and i + 1 < len(_inp_str_splitted):
+            next_word = _inp_str_splitted[i + 1]
             joined = word + next_word
 
             if joined.strip() in _vacob:
@@ -32,7 +32,7 @@ def cleaning_fix_unwanted_space_v2(_inp_str: str) -> str:
                 i += 1
             else:
                 if (i - 1) > 0:
-                    prev_word = _inp_str_splitted[i-1]                
+                    prev_word = _inp_str_splitted[i - 1]
                     joined = prev_word + word
 
                     if joined.strip() in _vacob:
@@ -45,9 +45,8 @@ def cleaning_fix_unwanted_space_v2(_inp_str: str) -> str:
     return " ".join(out_words)
 
 
-
 def split_string_into_chunks(input_string, max_tokens=4096):
-    encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
+    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
     tokens = encoding.encode(input_string)
 
     chunks = []
@@ -71,13 +70,11 @@ def split_string_into_chunks(input_string, max_tokens=4096):
                 current_chunk = []
                 current_chunk.append(token)
 
-
     return chunks
 
 
 def get_batches(transcript: List[str], token_limit):
     transcript_str = "".join(transcript)
-
 
     # estimate size of the transcript
     encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
@@ -86,18 +83,22 @@ def get_batches(transcript: List[str], token_limit):
 
     # output should be shorter then the input, so we can split 60% input, 40% output
     max_input_tokens = int(token_limit * 0.6)
-    
+
     # how many chunks we will need
     num_chunks = math.ceil(num_tokens / max_input_tokens)
 
     if num_chunks <= 1:
-        logger.info(f"Transcript is small enough to fit in one batch: tokens={num_tokens}")
+        logger.info(
+            f"Transcript is small enough to fit in one batch: tokens={num_tokens}"
+        )
         return num_tokens, [cleaning_fix_unwanted_space_v2(transcript_str)]
 
     # the limit of characters in one batch
     token_limit_per_batch = int(len(tokens) / num_chunks)
 
-    logger.info(f"Transcript is too big, splitting into {num_chunks} chunks, {token_limit_per_batch} tokens per batch!")
+    logger.info(
+        f"Transcript is too big, splitting into {num_chunks} chunks, {token_limit_per_batch} tokens per batch!"
+    )
 
     # lets create batches
     batches = []
@@ -115,8 +116,9 @@ def get_batches(transcript: List[str], token_limit):
             current_batch_size = len_line
     if len(current_batch) > 0:
         batches.append(cleaning_fix_unwanted_space_v2("\n".join(current_batch)))
-    
+
     return num_tokens, batches
+
 
 def get_video_script(url):
 
@@ -124,20 +126,21 @@ def get_video_script(url):
         raise ValueError("YouTube video url is required")
 
     parsed_url = urlparse(url)
-    
+
     query_params = parse_qs(parsed_url.query)
 
     video_id = None
-    
-    if 'v' in query_params:
-        video_id = query_params['v'][0]
+
+    if "v" in query_params:
+        video_id = query_params["v"][0]
     else:
-        raise ValueError("YouTube video ID not found in the URL") 
-    
+        raise ValueError("YouTube video ID not found in the URL")
+
     if not video_id:
         raise ValueError("YouTube video ID not found in the URL")
-    
 
-    scripts = YouTubeTranscriptApi.get_transcript(video_id, languages=("en", "en-US", "en-GB"))
+    scripts = YouTubeTranscriptApi.get_transcript(
+        video_id, languages=("en", "en-US", "en-GB")
+    )
 
-    return [item['text'] for item in scripts]
+    return [item["text"] for item in scripts]
