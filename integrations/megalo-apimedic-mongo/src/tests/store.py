1322bafb6dc167474f9155ff198cfa3eb75b8e10
Shubham Tiwary <67773966+ShubhamTiwary914@users.noreply.github.com>
2024-03-16 07:37:08 +0530

    Update store.py

--- integrations/megalo-apimedic-mongo/src/tests/store.py
+++ integrations/megalo-apimedic-mongo/src/tests/store.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-URI = 'https://dev.virtualearth.net/REST/v1/LocalSearch/?query=Hospitals&userLocation=40.785091,-73.968285,20000&key=Ar2PnnN71Ber0cEAkzbL41ForcU9ZPl-rEdO6MnMkvQNht9C8Buz8XQrcKTWlral'
+URI = ''
 jsonRes = json.loads((requests.get(URI)).text)
 
 
