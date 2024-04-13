b5d4b22f51e0854bec73255cd6cd9a92b716eeeb
Sneha-S-D <133686184+Sneha-S-D@users.noreply.github.com>
2024-04-13 11:24:02 +0530

    Team Tech_Titans
    just another PR .
    I just a baby....

---
+++ integrations/Spotify API with uAgents/agentverse.py
@@ -0,0 +1,36 @@
+from pydantic import Field
+from ai_engine import UAgentResponse, UAgentResponseType
+
+class Spotify(Model):
+    Singer: str =Field(description="Enter Singer")
+    genre: str=Field(description="Enter A Genre")
+
+
+
+Spotify_protocol = Protocol("Spotify")
+
+
+@Spotify_protocol.on_message(model=Spotify, replies={UAgentResponse})
+async def Search(ctx: Context, sender: str, msg: Spotify):
+    
+     
+    url = "https://spotify23.p.rapidapi.com/search/"    
+    querystring = {"q":{msg.Singer},"type":{msg.genre},"offset":"0","limit":"10","numberOfTopResults":"5"}
+
+    headers = {
+	"X-RapidAPI-Key": "API KEY ",
+	"X-RapidAPI-Host": "spotify23.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    file=response.json()
+        
+    for album in file['albums']['items']:
+            album_name = album['data']['name']
+            album_artist = ', '.join(artist['profile']['name'] for artist in album['data']['artists']['items'])
+            album_year = album['data']['date']['year']
+            message = f"Album: {album_name}\nArtist(s): {album_artist}\nYear: {album_year}"
+            await ctx.send(
+        sender, UAgentResponse(message=message, type=UAgentResponseType.FINAL)
+    )
+            break
+agent.include(Spotify_protocol, publish_manifest=True)
