3e9167fae6c053b855cdf3ad0ec066cd0d427f89
darthtakt <149038266+darthtakt@users.noreply.github.com>
2024-04-13 11:52:28 +0530

    Update model.py

--- integrations/movierating/src/message/model.py
+++ integrations/movierating/src/message/model.py
@@ -1,8 +1,10 @@
-# import required libraries
+#  import required libraries
 from uagents import Model
-
+from pydantic import Field
 class Message(Model):
     message : str
-
 class Response(Model):
     response : str
+
+class Movie(Model):
+    title : str = Field(description="Enter the movie name")
