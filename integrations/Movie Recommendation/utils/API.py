a042b122fb94e249df03f205cedc5a13ecc8b0da
SEGRR <segrr2003@gmail.com>
2024-04-20 23:36:10 +0530

    final commit

--- integrations/Movie Recommendation/utils/API.py
+++ integrations/Movie Recommendation/utils/API.py
@@ -1,9 +1,9 @@
 TMDB_HEADER = {
         "accept": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOWE0N2E2ZTllN2ZkNGVlMjdjNTJkYmNlODM2MmQwMyIsInN1YiI6IjY1MzNkOTU4YzhhNWFjMDExY2VmOTU5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.THVVPAru4KrtGi4zhYLHVNLDhQ_Nw43z2n4EdEmV1Lk"
+        "Authorization": "Bearer YOUR-BEARER-TOKEN"
     }
 
 RAPID_API_HEADER = {
-	    "X-RapidAPI-Key": "18fa0248e1msha84e65b69a3cd8cp1876eejsn5ee4ae881673",
+	    "X-RapidAPI-Key": "YOUR-API-KEY-HERE",
 	    "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com"
     }
