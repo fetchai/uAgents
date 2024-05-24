a9198fc701f14e7966b1437260a4b7136e257a57
Archento <crossmail@gmx.de>
2024-05-24 17:33:44 +0200

    fix: agent examples for new context

--- python/examples/14-broadcast/main.py
+++ python/examples/14-broadcast/main.py
@@ -22,7 +22,9 @@ proto = Protocol(name="proto", version="1.0")
 
 @proto.on_message(model=BroadcastExampleRequest, replies=BroadcastExampleResponse)
 async def handle_request(ctx: Context, sender: str, _msg: BroadcastExampleRequest):
-    await ctx.send(sender, BroadcastExampleResponse(text=f"Hello from {ctx.name}"))
+    await ctx.send(
+        sender, BroadcastExampleResponse(text=f"Hello from {ctx.agent.name}")
+    )
 
 
 # include protocol
