b85efdb162d642649af14be42404c8862548a791
Archento <crossmail@gmx.de>
2024-07-10 10:27:22 +0200

    fix(core): digest backwards compatibility (#451)
    Co-authored-by: lokman <lokman.rahmani@fetch.ai>

--- python/src/uagents/__init__.py
+++ python/src/uagents/__init__.py
@@ -1,4 +1,4 @@
 from .agent import Agent, Bureau  # noqa
 from .context import Context  # noqa
-from .models import Model  # noqa
+from .models import Model, Field  # noqa
 from .protocol import Protocol  # noqa
