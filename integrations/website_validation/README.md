086f0b4a36976d15acd2ad895ea0643011f873ce
pratrivedi <devpradhumn@gmail.com>
2024-07-16 19:05:05 +0530

    feat: add website validation agent in integrations

---
+++ integrations/website_validation/README.md
@@ -0,0 +1,59 @@
+# Website Validation
+
+Ensuring the accuracy and functionality of your website is crucial for maintaining a professional online presence. Whether you're a business owner, a developer, or a content creator, having a tool that can efficiently validate your website is invaluable. That's why I've harnessed the power of Crew AI agent technology to develop a comprehensive website validation tool.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [How It Works](#how-it-works)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+**Website Validator** is an innovative tool that leverages Crew AI agent technology to ensure your website is error-free and fully functional. This system checks for typos and identifies broken or invalid links, providing a detailed report to help you maintain a high-quality website.
+
+## Features
+
+- **Typo Detection**: The CrewAI agent scans your website content to identify and suggest corrections for any typos.
+- **Broken Link Detection**: The tool checks for broken or invalid links throughout your website, ensuring all links are functional.
+- **Comprehensive Reporting**: Receive detailed reports highlighting typos and broken links, allowing you to address issues promptly.
+
+## How It Works
+
+The Website Validator works through a seamless process to ensure your website is in top condition:
+
+1. **Input Website URL**: Enter the URL of the website you want to validate.
+2. **Check for Broken Links**: The tool first scans the website for any broken or invalid links, ensuring all hyperlinks are functional.
+3. **Scrape Website Data**: The tool scrapes the website content to prepare it for typo detection.
+4. **Typo Detection**: The CrewAI agent analyzes the scraped data to identify and suggest corrections for any typos.
+
+## Installation
+
+To get started with **Startup Idea Analyser**, follow these steps:
+
+1. **Clone the repository**:
+   ```bash
+   git clone https://github.com/fetchai/uAgents.git
+   cd website_validation
+
+2. **Set up .env file**:
+   To run the demo, you need to add these API keys in .env file:
+   ```bash
+   AGENT_MAILBOX_KEY = "YOUR_MAILBOX_KEY"
+   OPEN_AI_BASE_URL="YOUR_OPEN_AI_BASE_URL"
+   OPEN_AI_API_KEY="YOUR_OPEN_AI_BASE_KEY"
+   OPEN_AI_MODEL_NAME="YOUR_OPEN_AI_MODEL_NAME"
+3. **Open a shell**:
+   ```bash
+   poetry shell
+
+4. **Install dependencies using Poetry**:
+   ```bash
+   poetry install --no-root
+5. **Run the agent**:
+   ```bash
+   poetry run python agent.py
---
+++ integrations/website_validation/agent.py
@@ -0,0 +1,73 @@
+import os
+
+from ai_engine import UAgentResponse, UAgentResponseType
+from dotenv import load_dotenv
+from uagents import Agent, Context, Field, Model, Protocol
+from uagents.setup import fund_agent_if_low
+
+from crewai_grammer_check import find_content_grammar_mistakes, scrape_website
+from utils import find_broken_links, format_errors
+
+load_dotenv()
+
+AGENT_MAILBOX_KEY = os.getenv("AGENT_MAILBOX_KEY")
+
+agent = Agent(
+    name="website validation",
+    seed="website validation phrase",
+    mailbox=f"{AGENT_MAILBOX_KEY}@https://agentverse.ai",
+)
+
+protocol = Protocol("website validation versions", version="0.1.1")
+
+fund_agent_if_low(agent.wallet.address())
+
+print(agent.address, "agent_address")
+
+
+class WebsiteValidationInput(Model):
+    website_url: str = Field(
+        description="Describes the field where user provide the Website link that user want to find for website validation"
+    )
+
+
+@protocol.on_message(model=WebsiteValidationInput, replies={UAgentResponse})
+async def message_handler(ctx: Context, sender: str, msg: WebsiteValidationInput):
+    ctx.logger.info(f"Received webiste {sender} url: {msg.website_url}.")
+    try:
+        broken_links, invalid_links = find_broken_links(url=msg.website_url)
+        scrapped_content = scrape_website(url=msg.website_url)
+        grammar_mistakes = find_content_grammar_mistakes(content=scrapped_content)
+
+        if scrapped_content and grammar_mistakes:
+            res = format_errors(grammar_mistakes)
+            result = ""
+            if not broken_links or not invalid_links:
+                result = f"No broken links found for {msg.website_url}\n Grammer mistake:\n {res}\n"
+                await ctx.send(
+                    sender,
+                    UAgentResponse(message=result, type=UAgentResponseType.FINAL),
+                )
+            else:
+                result = f"Broken links: {broken_links} and Invalid links: {invalid_links}\n Grammer mistake:\n {res}\n"
+                await ctx.send(
+                    sender,
+                    UAgentResponse(message=result, type=UAgentResponseType.FINAL),
+                )
+        else:
+            result = f"No data found for {msg.website_url}"
+            await ctx.send(
+                sender, UAgentResponse(message=result, type=UAgentResponseType.FINAL)
+            )
+    except Exception as e:
+        ctx.logger.error(f"Somethig went wrong while finding borken links. {e}")
+        result = "Somethig went wrong while finding borken links."
+        await ctx.send(
+            sender, UAgentResponse(message=result, type=UAgentResponseType.ERROR)
+        )
+
+
+agent.include(protocol, publish_manifest=True)
+
+if __name__ == "__main__":
+    agent.run()
