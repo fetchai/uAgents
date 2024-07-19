7de6d1f86a25b4b0d9da9de7bc56fd4363377b02
Archento <crossmail@gmx.de>
2024-07-19 12:01:28 +0200

    chore: unify get_endpoints() output types

--- python/src/uagents/network.py
+++ python/src/uagents/network.py
@@ -190,7 +190,7 @@ class AlmanacContract(LedgerContract):
 
         return (expiry - height) * AVERAGE_BLOCK_INTERVAL
 
-    def get_endpoints(self, address: str) -> Optional[List[AgentEndpoint]]:
+    def get_endpoints(self, address: str) -> List[AgentEndpoint]:
         """
         Get the endpoints associated with an agent's registration.
 
@@ -204,7 +204,7 @@ class AlmanacContract(LedgerContract):
         response = self.query(query_msg)
 
         if not response["record"]:
-            return None
+            return []
 
         endpoints = []
         for endpoint in response.get("record")[0]["record"]["service"]["endpoints"]:
