d39dafddbf347296f42416aad0451fc18010a755
Archento <crossmail@gmx.de>
2024-08-13 16:11:54 +0200

    revert to full pydanticV2 BaseModel

--- python/src/uagents/models.py
+++ python/src/uagents/models.py
@@ -1,26 +1,18 @@
 import hashlib
-from typing import Any, Type, Union
+import json
+from typing import Type, Union
 
-from pydantic.v1 import BaseModel, Field  # noqa
+from pydantic import BaseModel
 
 
-# reverting back to pydantic v1 BaseModel for backwards compatibility
 class Model(BaseModel):
-    @classmethod
-    def model_json_schema(cls) -> str:
-        return cls.schema_json()
-
-    def model_dump_json(self) -> str:
-        return self.json()
-
-    @classmethod
-    def model_validate_json(cls, obj: Any) -> "Model":
-        return cls.parse_raw(obj)
-
     @staticmethod
     def build_schema_digest(model: Union["Model", Type["Model"]]) -> str:
-        schema = model.schema_json(indent=None, sort_keys=True)
-        digest = hashlib.sha256(schema.encode("utf8")).digest().hex()
+        schema = model.model_json_schema()
+        if "required" in schema and isinstance(schema["required"], list):
+            schema["required"].sort()
+        dump = json.dumps(schema, indent=None, sort_keys=True)
+        digest = hashlib.sha256(dump.encode("utf8")).digest().hex()
 
         return f"model:{digest}"
 
