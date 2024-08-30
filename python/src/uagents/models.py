18b35e693170c208317bacde2f7d7e08b17a1b44
Archento <crossmail@gmx.de>
2024-08-30 17:21:05 +0200

    add more wip

--- python/src/uagents/models.py
+++ python/src/uagents/models.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+from dataclasses import dataclass
 from typing import Literal, Type, Union
 
 from pydantic import BaseModel
@@ -7,6 +8,7 @@ from pydantic import BaseModel
 BASE_CLASSES = Literal["str", "int", "float", "bool", "list", "dict", "object"]
 
 
+@dataclass
 class PropertyField:
     types: list[BASE_CLASSES]
     title: str
@@ -15,21 +17,49 @@ class PropertyField:
     default: str
 
 
+@dataclass
+class Schema:
+    title: str
+    description: str
+    type: BASE_CLASSES
+    properties: dict[str, PropertyField]
+    required: list[str]
+
+
+# slightly different schema V
+#                           V
+# pydantic schema > custom schema (JSON Schema similiar) > calculate digest
+
+
 class Model(BaseModel):
+    _schema = None
+
     # This is a WIP - please ignore for now
     @classmethod
     def build_schema(cls) -> dict:
-        """Build a schema for a model."""
+        """
+        Build a schema for a model.
+
+        Targeting JSON schema format. similar but not the same
+        """
+        # enforce Fetch specification
         schema = cls.model_json_schema()
+
+        # cls.model_fields_set
+
         schema.pop("description", None)
         schema.pop("type", None)
         if "required" in schema and isinstance(schema["required"], list):
             schema["required"].sort()
+        # for field in ...:
+        #     pass
         return schema
 
     @staticmethod
     def build_schema_digest(model: Union["Model", Type["Model"]]) -> str:
         """Build a unique identifier for a model schema."""
+        # schema = model.build_schema()
+
         schema = model.model_json_schema()
         if "required" in schema and isinstance(schema["required"], list):
             schema["required"].sort()
