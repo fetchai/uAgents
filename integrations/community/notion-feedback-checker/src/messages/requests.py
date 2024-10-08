f8117b4da70f60e8897b10638033e641d476a10a
Joshua Croft <32483134+devjsc@users.noreply.github.com>
2024-10-08 11:38:41 +0100

    refactor(integration): removing community folder, integrations now at: https://github.com/fe… (#543)
    Co-authored-by: Joshua Croft <joshuacroft@Joshuas-MacBook-Pro.local>

--- integrations/community/notion-feedback-checker/src/messages/requests.py
+++
@@ -1,8 +0,0 @@
-from pydantic import Field
-from uagents import Model
-
-
-class NotionRequest(Model):
-    feedback_title: str = Field(
-        description="Feedback title that the user wants to use to create a new Notion feedback. User MUST specify it, don't pre fill it."
-    )
