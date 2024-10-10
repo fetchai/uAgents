fc4df92983d06f8a5bdfc5e5ac994b67d208d7dc
Archento <crossmail@gmx.de>
2024-10-10 11:24:06 +0200

    refactor agent metadata

--- python/examples/99-testing/agent.py
+++ python/examples/99-testing/agent.py
@@ -1,17 +1,30 @@
-from uagents.experimental.mobility import MobilityAgent
-from uagents.types import AgentGeoLocation
+# from uagents.experimental.mobility import MobilityAgent
+from uagents import Agent
 
-agent = MobilityAgent(
+# agent = MobilityAgent(
+#     seed="5813827564197286315342489173672541637287328671",
+#     location=AgentGeoLocation(
+#         latitude=52.507410,
+#         longitude=13.378240,
+#         radius=50,
+#     ),
+#     port=8000,
+#     endpoint="http://localhost:8000/submit",
+# )
+
+agent = Agent(
     seed="5813827564197286315342489173672541637287328671",
-    location=AgentGeoLocation(
-        latitude=52.507410,
-        longitude=13.378240,
-        radius=50,
-    ),
+    metadata={
+        "latitude": 52.507410,
+        "longitude": 13.378240,
+        "radius": 50,
+        "mobility": True,
+        "useless_information": "This is a test",
+    },
     port=8000,
     endpoint="http://localhost:8000/submit",
 )
 
 if __name__ == "__main__":
-    print(agent.location)
-    agent.run()
+    print(agent.metadata)
+    # agent.run()
