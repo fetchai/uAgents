caa277069101f4fc1fbc2d342b7d857c77df370d
James Riehl <james.riehl@fetch.ai>
2025-01-31 11:51:25 +0000

    feat(core): add deployment examples

---
+++ python/deployment/hello-agent/agent.py
@@ -0,0 +1,31 @@
+import os
+
+from uagents import Agent, Context
+
+AGENT_NAME = os.environ.get("UAGENT_NAME", "agent")
+AGENT_SEED = os.environ.get("UAGENT_SEED")
+AGENT_TYPE = os.environ.get("UAGENT_TYPE", "custom")
+AGENT_ENDPOINT = os.environ.get("UAGENT_ENDPOINT", "http://127.0.0.1:8000/submit")
+
+
+agent = Agent(
+    name=AGENT_NAME,
+    seed=AGENT_SEED,
+    mailbox=AGENT_TYPE == "mailbox",
+    proxy=AGENT_TYPE == "proxy",
+    endpoint=AGENT_ENDPOINT,
+)
+
+
+@agent.on_event("startup")
+async def handle_startup(ctx: Context):
+    ctx.logger.info("Agent has started")
+
+
+@agent.on_interval(period=5)
+async def handle_periodic(ctx: Context):
+    ctx.logger.info("Agent is still alive")
+
+
+if __name__ == "__main__":
+    agent.run()
