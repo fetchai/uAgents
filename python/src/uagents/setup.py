52732ad4396dab4fde2ed4f4dd590bd58c070b0d
Archento <crossmail@gmx.de>
2025-03-10 15:14:29 +0100

    setup, types, utils and wallet messaging

--- python/src/uagents/setup.py
+++ python/src/uagents/setup.py
@@ -1,29 +1,30 @@
 """Agent's Setup."""
 
-import requests
+from logging import Logger
+
+from cosmpy.aerial.client import LedgerClient
+from cosmpy.aerial.faucet import FaucetApi
 from cosmpy.crypto.address import Address
 
-from uagents import Agent
 from uagents.config import TESTNET_REGISTRATION_FEE
 from uagents.network import get_faucet, get_ledger
 from uagents.utils import get_logger
 
-LOGGER = get_logger("setup")
+LOGGER: Logger = get_logger("setup")
 
 
-def fund_agent_if_low(wallet_address: str, min_balance: int = TESTNET_REGISTRATION_FEE):
+def fund_agent_if_low(
+    wallet_address: str, min_balance: int = TESTNET_REGISTRATION_FEE
+) -> None:
     """
     Checks the agent's wallet balance and adds testnet funds if it's below min_balance.
 
     Args:
         wallet_address (str): The wallet address of the agent.
-        min_balance (int): The minimum balance required.
-
-    Returns:
-        None
+        min_balance (int): The minimum balance required. Defaults to TESTNET_REGISTRATION_FEE.
     """
-    ledger = get_ledger()
-    faucet = get_faucet()
+    ledger: LedgerClient = get_ledger()
+    faucet: FaucetApi = get_faucet()
 
     agent_balance = ledger.query_bank_balance(Address(wallet_address))
 
@@ -34,28 +35,3 @@ def fund_agent_if_low(wallet_address: str, min_balance: int = TESTNET_REGISTRATI
             LOGGER.info("Adding testnet funds to agent...complete")
         except Exception as ex:
             LOGGER.error(f"Failed to add testnet funds to agent: {str(ex)}")
-
-
-def register_agent_with_mailbox(agent: Agent, email: str):
-    """
-    Registers the agent on a mailbox server using the provided email.
-
-    Args:
-        agent (Agent): The agent object to be registered.
-        email (str): The email address associated with the agent.
-
-    Returns:
-        None
-    """
-    mailbox = agent.mailbox
-    register_url = f"{mailbox['http_prefix']}://{mailbox['base_url']}/v1/auth/register"
-    resp = requests.post(
-        register_url,
-        json={"email": email, "agent_address": agent.address},
-        timeout=15,
-    )
-    if resp.status_code == 200:
-        LOGGER.info("Registered agent on mailbox server")
-        mailbox["agent_mailbox_key"] = resp.json()["agent_mailbox_key"]
-    else:
-        LOGGER.exception("Failed to register agent on mailbox server")
