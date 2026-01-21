d374374cf422dd8326c64b131e6bf78ec0118697
James Riehl <james.riehl@fetch.ai>
2026-01-21 10:52:47 +0000

    feat: add switch for concurrent message handling

--- python/src/uagents/agent.py
+++ python/src/uagents/agent.py
@@ -244,6 +244,7 @@ class Agent(Sink):
         _message_queue: Asynchronous queue for incoming messages.
         _message_tasks: A set for storing message handler tasks
             to prevent the GC from deleting them.
+        _handle_messages_concurrently (bool): Whether to handle incoming messages concurrently.
         _on_startup (list[Callable]): List of functions to run on agent startup.
         _on_shutdown (list[Callable]): List of functions to run on agent shutdown.
         _version (str): The version of the agent.
@@ -298,6 +299,7 @@ class Agent(Sink):
         avatar_url: str | None = None,
         publish_agent_details: bool = True,
         store_message_history: bool = False,
+        handle_messages_concurrently: bool = False,
     ):
         """
         Initialize an Agent instance.
@@ -330,6 +332,7 @@ class Agent(Sink):
             publish_agent_details (bool): Publish agent details to Agentverse on connection via
             local agent inspector.
             store_message_history (bool): Store the message history for the agent.
+            handle_messages_concurrently (bool): Whether to handle incoming messages concurrently.
         """
         self._init_done = False
         self._name = name
@@ -389,6 +392,7 @@ class Agent(Sink):
         self._dispenser = Dispenser(msg_cache_ref=self._message_history)
         self._message_queue = asyncio.Queue()
         self._message_tasks: set[asyncio.Task] = set()
+        self._handle_messages_concurrently = handle_messages_concurrently
         self._on_startup = []
         self._on_shutdown = []
         self._network = network
@@ -1380,17 +1384,26 @@ class Agent(Sink):
                     continue
 
             if handler is not None:
-                handler_task = asyncio.create_task(
-                    self._handle_message(
+                if self._handle_messages_concurrently:
+                    handler_task = asyncio.create_task(
+                        self._handle_message(
+                            handler=handler,
+                            context=context,
+                            sender=sender,
+                            model_class=model_class,
+                            message=recovered,
+                        )
+                    )
+                    self._message_tasks.add(handler_task)
+                    handler_task.add_done_callback(self._message_tasks.discard)
+                else:
+                    await self._handle_message(
                         handler=handler,
                         context=context,
                         sender=sender,
                         model_class=model_class,
                         message=recovered,
                     )
-                )
-                self._message_tasks.add(handler_task)
-                handler_task.add_done_callback(self._message_tasks.discard)
 
 
 class Bureau:
