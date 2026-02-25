955dc10c59838a69386c3145f2b414292949e6d1
zmezei <mezeienator@gmail.com>
2026-02-25 11:28:47 +0000

    import instead of deprecate

--- python/src/uagents/types.py
+++ python/src/uagents/types.py
@@ -12,7 +12,10 @@ from pydantic import (
 from typing_extensions import Self
 from uagents_core.envelope import Envelope
 from uagents_core.models import Model
-from uagents_core.types import AgentGeolocation, AgentMetadata  # noqa: F401
+from uagents_core.types import (  # noqa: F401 - these classes were defined in this file, keep it here for backwards compatibility!
+    AgentGeolocation,
+    AgentMetadata,
+)
 
 from uagents.config import (
     MESSAGE_HISTORY_MESSAGE_LIMIT,
